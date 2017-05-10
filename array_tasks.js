a2913dbdac2da56779caff7587aa47b39fac00d3
James Duncan <james.a.duncan91@gmail.com>
2017-05-10 15:35:39 +0100

    3 tests passing

--- array_tasks.js
+++ array_tasks.js
@@ -1,16 +1,28 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
+	concat: function (arr1, arr2) {
+		newArray = [];
+		for(var item of arr1){
+			newArray.push(item);
+		}
+		for(var item of arr2){
+			newArray.push(item);
+		}
+		return newArray;
+	},
 
-	// },
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+		return arr;
+	},
 
-	// insertAt: function (arr, itemToAdd, index) {
-		
-	// },
-
-	// square: function (arr) {
-		
-	// },
+	square: function (arr) {
+		newArray = [];
+		arr.forEach(function(element){
+			newArray.push(element * element);
+		})
+		return newArray;
+	},
 
 	// sum: function (arr) {
 		
--- tests.js
+++ tests.js
@@ -10,20 +10,20 @@ describe('Array tasks', function () {
 		assert.deepEqual(expectation, arrayTasks.concat(arr1, arr2))
 	})
 
-	// it('should insert an item in an array at any index position', function () {
-	// 	var arr = [1, 2, 4]
-	// 	assert.deepEqual([1, 2, 3, 4], arrayTasks.insertAt(arr, 3, 2))
-	// })
+	it('should insert an item in an array at any index position', function () {
+		var arr = [1, 2, 4]
+		assert.deepEqual([1, 2, 3, 4], arrayTasks.insertAt(arr, 3, 2))
+	})
 
-	// it('should square all values in an array, returning a new array', function () {
-	// 	var arr = [1, 2, 3, 4, 5]
-	// 	assert.deepEqual([1, 4, 9, 16, 25], arrayTasks.square(arr))
-	// })
+	it('should square all values in an array, returning a new array', function () {
+		var arr = [1, 2, 3, 4, 5]
+		assert.deepEqual([1, 4, 9, 16, 25], arrayTasks.square(arr))
+	})
 
-	// it('should calculate the sum of all values in an array', function () {
-	// 	var arr = [1, 2, 3, 4, 5]
-	// 	assert.equal(15, arrayTasks.sum(arr))
-	// })
+	it('should calculate the sum of all values in an array', function () {
+		var arr = [1, 2, 3, 4, 5]
+		assert.equal(15, arrayTasks.sum(arr))
+	})
 
 	// it('should find duplicate values in an array, returning a new array of the duplicates', function () {
 	// 	var arr = [1, 2, 3, 4, 4, 5, 5, 5]
